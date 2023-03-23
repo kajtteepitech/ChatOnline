55e760f3c27b9e8161c3f97fb3aabc8220d32009
kajtteepitech <ilia.schastnev@epitech.eu>
2023-03-23 17:24:50 +0100

    added a smooth scrolling and a clear conversation method for debug

--- chatonline/src/ChatRoom.js
+++ chatonline/src/ChatRoom.js
@@ -11,6 +11,8 @@ class ChatRoom extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClearConversation = this.handleClearConversation.bind(this);
+    this.messagesEndRef = React.createRef();
   }
 
   componentDidMount() {
@@ -21,11 +23,15 @@ class ChatRoom extends React.Component {
     this.socket.on('chat message', (message) => {
       this.setState((prevState) => ({
         messages: [...prevState.messages, message],
-      }));
+      }), () => {
+        this.scrollToBottom();
+      });
     });
 
     this.socket.on('message history', (history) => {
-      this.setState({ messages: history });
+      this.setState({ messages: history }, () => {
+        this.scrollToBottom();
+      });
     });
   }
 
@@ -43,11 +49,21 @@ class ChatRoom extends React.Component {
         this.setState((prevState) => ({
           messages: [...prevState.messages, messageToSend],
           messageInput: '',
-        }));
+        }), () => {
+          this.scrollToBottom();
+        });
       } else {
         console.error('Message was not confirmed by server.');
       }
     });
+  }
+
+  handleClearConversation() {
+    this.setState({ messages: [] });
+  }
+
+  scrollToBottom() {
+    this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
   }
 
   render() {
@@ -55,21 +71,23 @@ class ChatRoom extends React.Component {
       <div className={styles.ChatRoom}>
         <ul className={styles.Messages}>
           {this.state.messages.map((message, index) => (
-            <li className={styles.Message} key={index}>{message}</li>
+            <li key={index} className={styles.Message}>{message}</li>
           ))}
+          <div ref={this.messagesEndRef}></div>
         </ul>
-        <form className={styles.Form} onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className={styles.Form}>
           <input
-            className={styles.Input}
             type="text"
-            placeholder="Type your message here..."
             value={this.state.messageInput}
             onChange={(event) =>
               this.setState({ messageInput: event.target.value })
             }
+            className={styles.Input}
           />
-          <button className={styles.Button} type="submit">Send</button>
+          <button type="submit" className={styles.Button}>Send</button>
         </form>
+        <p></p>
+        <button onClick={this.handleClearConversation} className={styles.ClearButton}>Clear the conversation history</button>
       </div>
     );
   }
